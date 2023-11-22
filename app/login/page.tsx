074480fa3f51af87abf0abf65c429844b1d10ec7
burnchi <1273732363@qq.com>
2023-11-22 23:55:05 +0800

    add login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
 
 const Loginpage = () => {
-  return (
-    <div className=' w-screen h-screen justify-center items-center'>
-        
-    </div>
-  )
+    return (
+        <div className=' w-screen h-screen justify-center items-center flex'>
+            <form className='min-w-[400px]  p-10 flex flex-col gap-y-4 border'>
+                <h1 className=' text-center text-2xl font-semibold mb-4'>登录界面</h1>
+                <input
+                    type='text'
+                    placeholder='账号'
+                    name='account'
+                    className='border h-[5vh] px-3 outline-none'
+                    />
+                <input
+                    type='password'
+                    placeholder='密码'
+                    name='pwd'
+                    className='border h-[5vh] px-3 outline-none'
+                />
+                {/* 提交按钮 */}
+                <button
+                    // 如果没登录禁止提交
+                    type='submit'
+                    className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring focus:border-blue-300 w-full disabled:bg-gray-400'
+                >
+                    Submit
+                </button>
+            </form>
+        </div>
+    )
 }
 
 export default Loginpage
